6421aaa7b96bbc879ff2b3986a8446018bbff98f
hhj <1291536750@qq.com>
2020-10-22 16:50:43 +0800

    test the cloud server

--- Data lab/datalab-handout/bits.c
+++ Data lab/datalab-handout/bits.c
@@ -143,7 +143,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  return ((~ (x & y)) & (~ (~x & ~y)));
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -152,9 +152,7 @@ int bitXor(int x, int y) {
  *   Rating: 1
  */
 int tmin(void) {
-
-  return 2;
-
+  return (1 << 0x1f);
 }
 //2
 /*
@@ -165,7 +163,7 @@ int tmin(void) {
  *   Rating: 1
  */
 int isTmax(int x) {
-  return 2;
+  return x & (~(1 << 0x1f));
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
